5ebe5ea092ca2f9057a5688e52df4d4a1e1b5429
Naveen-gn <naveen2003gn@gmail.com>
2024-02-11 22:58:16 +0530

    add redux toolkit

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -2,12 +2,15 @@ import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import { useState } from 'react'
+import { useDispatch,useSelector } from 'react-redux';
+import { signInStart,signInSuccess,signInFailure } from '../redux/user/userSlice';
 
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading,error:errorMessage}=useSelector(state=>state.user)
+  const user=useSelector(state=>state.user)
+  const dispatch = useDispatch();
   const navigate=useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });     
@@ -15,12 +18,10 @@ export default function SignIn() {
   const handleSubmit=async(e)=>{
     e.preventDefault()
     if (!formData.email || !formData.password) {
-      setErrorMessage('Please fill all the fields');
-      return;
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+     dispatch(signInStart());
       const res=await fetch('/api/auth/signin',{
         method:'POST',
         headers:{
@@ -30,15 +31,14 @@ export default function SignIn() {
       });
       const data=await res.json()
       if (data.success===false) {
-        setLoading(false);
-       return setErrorMessage(data.message);
+       return dispatch(signInFailure(data.message))
       }
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/')
       }
     } catch (error) {
-      setErrorMessage(data.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message))
     }
   };
   return (
