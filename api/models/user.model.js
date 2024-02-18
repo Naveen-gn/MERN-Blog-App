3f49078ab6ef503f51d45c68cc72e0d88b7f697b
Naveen-gn <naveen2003gn@gmail.com>
2024-02-18 17:29:35 +0530

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,25 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     profilePicture: {
-        type: String,
-        default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Dmy%2Bprofile%2Bicon&psig=AOvVaw0w3VR_YuBupx4bae6Vdxyi&ust=1707800646644000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCKC3nK2DpYQDFQAAAAAdAAAAABAE"
-    }
-}, { timestamps: true });
+      type: String,
+      default:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
