ff7ac3a74acaf9b3d3e2055e164836b62af8e6cf
Naveen-gn <naveen2003gn@gmail.com>
2024-02-19 13:44:39 +0530

    index js modified

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-  app.use(express.static(path.join(__dirname,'/client')));
+  app.use(express.static(path.join(__dirname,'/client/dist')));
   //app.use(express.static(path.join(__dirname, 'client')));
 
 app.get('*', (req, res) => {
