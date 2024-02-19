b7048826d7688e13534748b575e87490fbcb3a41
Naveen-gn <naveen2003gn@gmail.com>
2024-02-19 16:40:25 +0530

    build to dist

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
-    outDir: 'build' // Specify the output directory
+    outDir: 'dist' // Specify the output directory
   }
 })
