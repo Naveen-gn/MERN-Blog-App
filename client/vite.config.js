943976db1185da24c859adfd40b37d6426d5eaa8
Naveen-gn <naveen2003gn@gmail.com>
2024-02-19 16:46:40 +0530

    vite conf file out dir change from dist to public

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
-    outDir: 'dist' // Specify the output directory
+    outDir: 'public' // Specify the output directory
   }
 })
