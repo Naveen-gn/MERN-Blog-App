3035e2d5321cde497eb2579fa22d2bf7c877dd6b
Naveen-gn <naveen2003gn@gmail.com>
2024-02-21 15:29:18 +0530

    undo changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,12 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  proxy: {
-    '/api': {
-      target: 'https://naveen-mern-blog-app-server.vercel.app',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
+  server: {
+    proxy: {
+      '/api': 'https://naveen-mern-blog-app-server.vercel.app',
+      secure: false,
     },
   },
-  // server: {
-  //   proxy: {
-  //     '/api': '',
-  //     secure: false,
-  //   },
-  // },
   plugins: [react()],
   build: {
     outDir: './build',
