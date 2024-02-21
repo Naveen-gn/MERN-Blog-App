0fa51e633992991060d361965e9da612a0f29f8e
Naveen-gn <naveen2003gn@gmail.com>
2024-02-21 14:46:30 +0530

    removed cors

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -23,7 +23,7 @@ export default function SignIn() {
     }
     try {
      dispatch(signInStart());
-      const res=await fetch('/api/api/auth/signin',{
+      const res=await fetch('/api/auth/signin',{
         method:'POST',
         headers:{
           'Content-Type':'application/json'
