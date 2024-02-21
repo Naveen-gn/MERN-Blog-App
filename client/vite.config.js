db30d109e2047bbd673bfb91192e78f87967c7e2
Naveen-gn <naveen2003gn@gmail.com>
2024-02-21 15:24:00 +0530

    vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,19 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': 'https://naveen-mern-blog-app-server.vercel.app',
-      secure: false,
+  proxy: {
+    '/api': {
+      target: 'https://naveen-mern-blog-app-server.vercel.app',
+      changeOrigin: true,
+      rewrite: (path) => path.replace(/^\/api/, ''),
     },
   },
+  // server: {
+  //   proxy: {
+  //     '/api': '',
+  //     secure: false,
+  //   },
+  // },
   plugins: [react()],
   build: {
     outDir: './build',
