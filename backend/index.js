209830e8e483affbdba473d06cfd8fc91784a0ed
Naveen-gn <naveen2003gn@gmail.com>
2024-02-21 18:43:11 +0530

    updated the cors

--- backend/index.js
+++ backend/index.js
@@ -11,44 +11,31 @@ import cors from 'cors';
 
 dotenv.config();
 
-
-
 mongoose
   .connect(process.env.MONGO)
-  .then(() => {
-    console.log('MongoDb is connected');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-const __dirname = path.resolve();
+  .then(() => console.log('MongoDB is connected'))
+  .catch(err => console.error(err));
 
 const app = express();
+
 app.use(cors());
-app.options("*", corsOptionsDelegate);
+
 app.use(express.json());
 app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
 app.get("/", (req, res) => {
-  res.json({
-    message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄",
-  });
+  res.json({ message: "🦄🌈✨👋🌎🌍🌏✨🌈🦄" });
 });
-
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/comment', commentRoutes);
 
-app.use(express.static(path.join(__dirname,'/client/dist')));
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
@@ -56,10 +43,13 @@ app.get('*', (req, res) => {
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
     success: false,
     statusCode,
-    message,
+    message: err.message || 'Internal Server Error',
   });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
